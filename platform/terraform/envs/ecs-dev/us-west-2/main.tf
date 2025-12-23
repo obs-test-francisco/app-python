496f7cfac375c75316dec509e27708bd299de203
Francisco Gray <francisco.gray@observeinc.com>
2025-12-22 16:55:46 -0800

    fix: slight restructure/doc update

---
+++ platform/terraform/envs/ecs-dev/us-west-2/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "app" {
+  source = "../../modules/aws-ecs-fargate/"
+
+  image_url     = "obsfrancisco/otel-python-app"
+  image_version = "v0.46.0"
+}
